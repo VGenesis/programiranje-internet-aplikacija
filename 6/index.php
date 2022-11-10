efdca5f0fd9da9174c3cdeeaa31fae59e915f3c3
VGenesis <91892895+VGenesis@users.noreply.github.com>
2022-11-10 17:57:05 +0100

    Cas 6

---
+++ 6/index.php
@@ -0,0 +1,58 @@
+<?php
+    // $a = array('a' => 1, 'b' => 2, 'c' => array('a' => 'ananas', 'b' => 'banana'));
+    // var_dump($a);
+
+    // $quantity = 1000;
+    // $originalPrice = 100;
+    // $currentPrice = 25;
+
+    // $priceDifference = $originalPrice - $currentPrice;
+    // $pricePercentDifference = (-$priceDifference) * 100 /$originalPrice;
+
+    // $str1 = 'the';
+    // $str2 = 'games';
+    // $str3 = 'begin';
+    // $str4 = 'now';
+    
+    // $str = $str1 . ' ' . $str2 . ' ' . $str3 . ' ' . $str4;
+    // print $str;
+
+    
+?>
+
+<!-- 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PIA 6</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <div>
+            <table>
+                <tr>
+                    <td>Quantity</td>
+                    <td>Original price</td>
+                    <td>Current price</td>
+                    <td>Price difference</td>
+                    <td>Percent difference</td>
+                </tr>
+                <tr align="center">
+                    <td><?php echo $quantity?></td>
+                    <td><?php echo $originalPrice?></td>
+                    <td><?php echo $currentPrice?></td>
+                    <td><?php echo $priceDifference?></td>
+                    <td><?php echo $pricePercentDifference?>%</td>
+                </tr>
+            </table>
+        </div>
+
+        <div>
+            <form action="message.php" method="post">
+
+            </form>
+        </div>
+        
+    </body>
+</html> -->
---
+++ 6/message.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <div>
+            <?php echo $_POST['name']?>, dobrodosli na sajt!
+        </div>
+    </body>
+</html> 
